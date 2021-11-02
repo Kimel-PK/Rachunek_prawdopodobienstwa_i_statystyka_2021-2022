c790cda6aacffdecbc0e6a907c8242313c7d621c
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2021-11-02 10:55:34 +0100

    Zadanie 2.7 treść i zarys

---
+++ Zadanie 2.7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using AwokeKnowing.GnuplotCSharp;
+
+namespace Zadanie_2_7 {
+    class Program {
+        static void Main(string[] args) {
+            
+            // ziarno generatora jest określane na podstawie zegaru systemowego w momencie utworzenia obiektu klasy
+            Random random = new Random();
+            
+            // budujemy talie
+            List<Karta> talia = new List<Karta>();
+            for (int i = 0; i < 13; i++) {
+                talia.Add(Karta.Trefl);
+            }
+            for (int i = 0; i < 13; i++) {
+                talia.Add(Karta.Pik);
+            }
+            for (int i = 0; i < 13; i++) {
+                talia.Add(Karta.Karo);
+            }
+            for (int i = 0; i < 13; i++) {
+                talia.Add(Karta.Kier);
+            }
+            
+            
+            
+        }
+    }
+    
+    public enum Karta {
+        Trefl,
+        Pik,
+        Karo,
+        Kier
+    }
+    
+}
---
+++ Zadanie 2.7/README.md
@@ -0,0 +1,25 @@
+
+# Zestaw 2 - Zadanie 7
+
+## Treść
+
+Proszę dokonać numerycznej symulacji zadania 7 z zestawu 1.
+
+> Treść zadania 7 z zestawu 1  
+Wyciągamy trzy karty z tali 52 kart. Jakie jest prawdopodobieństwo, że żadna z
+nich nie będzie treflem?
+
+W tym celu:
+
+1. Proszę uruchomić generator liczb pseudolosowych wbudowany w używany język
+i (jeśli występuje) zapoznać sić z rolą ziarna generatora. W efekcie powinniście
+Państwo generować liczby pseudolosowe o rozkładzie jednorodnym w przedziale (0,1)
+różne dla kolejnych uruchomień programu.
+
+2. Zasymulować pojedyncze wyciąganie trzech kart z tali 52 zwracając informację
+czy wśród wylosowanych kart jest trefl.
+
+3. Powtórzyć losowanie z punktu 2 wielokrotnie, i zbadać ile powtórzeń losowania
+należy wykonać aby osiągnąć granicę (z dokładnością 0.1%) częstotliwości odpowiadającą prawdopodobieństwu wyznaczonemu w zadaniu 7 z zestawu 1.
+
+> Wynik wyznaczony w zadaniu 7 zestaw 1:
